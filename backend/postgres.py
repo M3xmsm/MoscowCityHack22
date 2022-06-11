a1412c24cd0f778ef52afb0a0c0a9c393be54407
ksetherg <ksetherg@gmail.com>
2022-06-11 10:05:00 +0300

    Added PostgreSQL connector

---
+++ backend/postgres.py
@@ -0,0 +1,34 @@
+from playhouse.db_url import connect
+from peewee import Model, TextField, CharField
+
+
+def get_postgres_connection():
+    psql_db = connect(
+        url='postgresext://guest:q1w2e3r4@rc1b-s3a3dtlkkxb1qz6y.mdb.yandexcloud.net:6432/db',
+        sslmode='require',
+        sslrootcert='CA.pem'
+    )
+    return psql_db
+
+
+class MoscowCompanies(Model):
+    company_name = CharField(primary_key=True)
+    company_url = CharField(null=True)
+    tel = CharField(null=True)
+    category = CharField(null=True)
+    description = TextField(null=True)
+
+    class Meta:
+        table_name = 'moscow_companies'
+
+
+def main():
+    psql_db = get_postgres_connection()
+    psql_db.bind([MoscowCompanies])
+
+    with psql_db:
+        MoscowCompanies.create(
+            company_name=...,
+            company_url=...,
+            description=...,
+        )
