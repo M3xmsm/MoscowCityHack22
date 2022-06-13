a30ecec313f215c7c306e0b7bf16a2949feb6b8f
ksetherg <ksetherg@gmail.com>
2022-06-13 09:11:34 +0300

    Added analytics api call

--- moscowhack/backend/main.py
+++ moscowhack/backend/main.py
@@ -3,7 +3,12 @@ from typing import Dict, List, Any
 from fastapi import FastAPI, Path
 from fastapi.middleware.cors import CORSMiddleware
 
-from moscowhack.backend.postgres import get_postgres_connection, CompaniesInfo, MoscowProducts
+from moscowhack.backend.postgres import (
+    get_postgres_connection,
+    CompaniesInfo,
+    MoscowProducts,
+    AnalyticsDataTry,
+)
 
 pg_client = get_postgres_connection()
 pg_client.bind([CompaniesInfo, MoscowProducts])
@@ -43,3 +48,15 @@ async def get_products_by_inn(
             return list(result.dicts())
         else:
             return []
+
+
+@app.get("/analytics/get/{inn}")
+async def get_analytics_by_inn(
+        inn: str = Path(None, min_length=10, max_length=12, regex="^[0-9]+$")
+) -> Dict[str, Any]:
+    with pg_client:
+        if inn is not None:
+            result = AnalyticsDataTry.select().where(AnalyticsDataTry.inn == inn)
+            return result.dicts()[0]
+        else:
+            return {}
